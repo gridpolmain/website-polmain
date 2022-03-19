21937903ee840e709ed1fc563add02fd9d764274
gridpolmain <101793014+gridpolmain@users.noreply.github.com>
2022-03-19 20:17:52 +0700

    Update navbar.php

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -70,12 +70,12 @@
               </div>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff;">Pendaftaran</a>
+              <a class="nav-link dropdown-toggle" href="<?= base_url();?>index.php/welcome/kerjasama" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff;">Pendaftaran</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="<?= base_url();?>index.php/welcome/pmb">Penerimaan Mahasiswa Baru</a>
               </div>
             </li>
-            <a class="nav-link" href="#" role="button"aria-haspopup="true" aria-expanded="false" style="color: #fff;">Penelitian & Pengabdian</a>
+            <a class="nav-link" href="<?= base_url();?>index.php/welcome/P3M" role="button"aria-haspopup="true" aria-expanded="false" style="color: #fff;">Penelitian & Pengabdian</a>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: #fff;">Layanan</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
